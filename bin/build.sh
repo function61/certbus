0d4802b10c1a70e30b6db4da6cad33bbc99df25a
Joonas Loppi <joonas@joonas.fi>
2020-02-16 22:30:06 +0200

    dev environment: add certbus shorthand

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -eu
 
+if [ ! -L "/usr/local/bin/certbus" ]; then
+	ln -s /workspace/rel/certbus_linux-amd64 /usr/local/bin/certbus
+fi
+
 source /build-common.sh
 
 BINARY_NAME="certbus"
