940038aaea1482220c3903eecc6a1639d97fdcc5
Joonas Loppi <joonas@joonas.fi>
2020-03-04 09:51:41 +0200

    package Lambda function

--- bin/build.sh
+++ bin/build.sh
@@ -9,6 +9,17 @@ source /build-common.sh
 BINARY_NAME="certbus"
 COMPILE_IN_DIRECTORY="cmd/certbus"
 
+# TODO: one deployerspec is done, we can stop overriding this from base image
+function packageLambdaFunction {
+	if [ ! -z ${FASTBUILD+x} ]; then return; fi
+
+	cd rel/
+	cp "${BINARY_NAME}_linux-amd64" "${BINARY_NAME}"
+	rm -f lambdafunc.zip
+	zip lambdafunc.zip "${BINARY_NAME}"
+	rm "${BINARY_NAME}"
+}
+
 standardBuildProcess
 
-# buildstep packageLambdaFunction
+buildstep packageLambdaFunction
